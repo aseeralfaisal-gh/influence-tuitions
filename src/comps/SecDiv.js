301a86c1c4a74cf3dcb86dd2285e83626b4982e7
aseeralfaisal-gh <aseeralfaisal@gmail.com>
2021-02-20 22:56:33 +0600

    fixed secdiv

--- src/comps/SecDiv.js
+++ src/comps/SecDiv.js
@@ -6,9 +6,9 @@ const SecDiv = () => {
         <div className='SecDiv'>
             <i className="far fa-lightbulb"></i>
             <div className='des'>
-                <h2 className='te'>Are you looking for qualified teacher?</h2>
-                <h3 className='te'>Then this is the right place for you</h3>
-                <h4 className='te'>We provide experienced tutors for students at home.</h4>
+                <h1 className='te'>Are you looking for qualified teacher?</h1>
+                <h2 className='te'>Then this is the right place for you</h2>
+                <h3 className='te'>We provide experienced tutors for students at home.</h3>
             </div>
         </div>
         </>
