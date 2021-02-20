d321401fd7e03f0c7ba45260cb1bda449264f51b
aseeralfaisal-gh <aseeralfaisal@gmail.com>
2021-02-20 21:17:59 +0600

    SecDiv updated

--- src/comps/SecDiv.js
+++ src/comps/SecDiv.js
@@ -1,12 +1,10 @@
 import './SecDiv.css';
-import bulb from '../logos/bulb-01.png';
 
 const SecDiv = () => {
     return (
         <>
         <div className='SecDiv'>
-            {/* <i className="far fa-hourglass"></i> */}
-            <img className='bulb' src={bulb} width='600rem' height='600rem' />
+            <i className="far fa-lightbulb"></i>
             <div className='des'>
                 <h2 className='te'>Are you looking for qualified teacher?</h2>
                 <h3 className='te'>Then this is the right place for you</h3>
